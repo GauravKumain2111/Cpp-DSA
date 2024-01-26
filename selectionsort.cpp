c348e367bfbd4db34b8f1cd85d53af5e4a9d3013
GauravKumain2111 <122596381+GauravKumain2111@users.noreply.github.com>
2024-01-26 11:17:22 +0530

    Create selectionsort.cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,20 @@
+//Using Selection SOrt
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        
+        for(int i =0;i<nums.size()-1;i++){
+            int min_index = i;
+
+            for(int j = i+1;j<nums.size();j++){
+                if(nums[j]<nums[min_index]){
+                    min_index = j;
+
+                }
+            }
+            swap(nums[i],nums[min_index]);
+        }
+     return nums;
+    }
+};
