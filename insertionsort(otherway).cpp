a05b87fca657cc3e5e626cd5cc43451d68504be2
GauravKumain2111 <122596381+GauravKumain2111@users.noreply.github.com>
2024-02-19 19:51:10 +0530

    Update insertionsort(otherway).cpp

--- insertionsort(otherway).cpp
+++ insertionsort(otherway).cpp
@@ -3,10 +3,13 @@ void insertionSort(int arr[], int n)
 {
     //write your code here
     for(int i =0;i<=n-1;i++){
-        for(int j=i;j>0;j--){
-            if(arr[j-1]>arr[j]){
-                std::swap(arr[j-1],arr[j]);
-            }
+        for(int j=i;j>0&&arr[j-1]>arr[j];j--){
+           
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            
+            
         }
     }
 }
