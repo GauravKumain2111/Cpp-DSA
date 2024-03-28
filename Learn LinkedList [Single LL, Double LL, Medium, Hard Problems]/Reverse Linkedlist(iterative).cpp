9575152dbb8618991ff98290619c3e66fb3a1a9a
GauravKumain2111 <122596381+GauravKumain2111@users.noreply.github.com>
2024-03-28 20:20:31 +0530

    Create Reverse Linkedlist(iterative).cpp

---
+++ Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Reverse Linkedlist(iterative).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+       ListNode* temp =head;
+       ListNode* prev = NULL;
+       while(temp!=NULL){
+        ListNode* front = temp->next;
+        temp->next = prev;
+        prev= temp;
+        temp=front;
+        
+
+       }
+       return prev;
+    }
+};
